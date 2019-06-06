5dc495035d0b0ff11f16823288c7bdf7ce01a7ed
mahdiou <mmahdiou.diallo@gmail.com>
2019-06-06 14:44:13 +0200

    add bihistogram plot

--- ch1_eda/s1_3_3/graphing_techniques.py
+++ ch1_eda/s1_3_3/graphing_techniques.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy
 
-
 def autocorrelation_plot(x: np.ndarray) -> np.ndarray:
     """
     Computes and graphs the auto correlation of the series `x`
     That is, it checks if there is an auto correlation between `x` 
     and its time lagged version for different values of the time lag
 
-    This can help check for the randomness of the data
+    This can help check for the randomness of the data.
+    It can also help decide if we can model the data using a linear regression
+    or a Box-Jenkins autoregressive model
 
     reference: [NIST Engineering Stats Handbook](https://www.itl.nist.gov/div898/handbook/eda/section3/autocopl.htm)
     """
@@ -22,7 +23,7 @@ def autocorrelation_plot(x: np.ndarray) -> np.ndarray:
         x_head = x[:N-h]
         x_tail = x[h:]
 
-        C_h = ((x_head-x_bar) * (x_tail-x_bar)).sum() / N # could also be divided by `(N-h)`
+        C_h = ((x_head-x_bar) * (x_tail-x_bar)).sum() / (N-h) # could either divide by `N` or `(N-h)`. the latter has less bias
 
         return C_h
 
@@ -45,7 +46,7 @@ def autocorrelation_plot(x: np.ndarray) -> np.ndarray:
     ax.axhline(y=th_99, color='r', linestyle=':')
     ax.axhline(y=-th_99, color='r', linestyle=':')
 
-    ax.plot(corr, linestyle='-')
+    ax.plot(corr, linestyle='-', marker='o')
 
     ax.set_xlabel('Lag')
     ax.set_ylabel('Autocorrelation')
@@ -56,8 +57,35 @@ def autocorrelation_plot(x: np.ndarray) -> np.ndarray:
     return corr
 
 
+def bihistogram_plot(x1: np.ndarray, x2: np.ndarray) -> None:
+    """
+    Plots a bihistogram of the data
+    The data can be from a single data set where we want to check the significance of a factor
+    It gives graphical answers to the following questions:
+        - Is a 2-level factor significant?
+        - Does a (2-level) factor have an effect?
+        - Does the location change between the 2 subgroups?
+        - Does the variation change between the 2 subgroups?
+        - Does the distributional shape change between subgroups?
+        - Are there any outliers?
+    It is a graphical equivalent of the following quantitative tests:
+        - 2 sample t-test (shift in location)
+        - Fisher test (shift in variation)
+        - Kolgomorov-Smirnov test (shift in distribution)
+        also
+        - quantile-quantile plot (shift in location and distribution)
+    """
+    _, bins, _ = plt.hist(x1, bins=50, density=True, label='x1')
+    _ = plt.hist(x2, bins=bins, alpha=0.5, density=True, label='x2')
+    plt.legend(loc='upper right')
+
+    plt.show()
+
 
 if __name__ == '__main__':
-    X = np.random.standard_normal(50)#np.arange(50)
-    # print(X)
+    # X = np.random.standard_normal(50)
+    # X = np.arange(50)
+    X = np.sin(np.linspace(0, 4*np.pi, 50))
+
+    
     autocorrelation_plot(X)
